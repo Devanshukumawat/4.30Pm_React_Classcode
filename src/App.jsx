c49f4d9671037d1fc4b983595cf80b7fff80dc34
Devanshukumawat <devanshukumawat063@gmail.com>
2024-03-29 17:46:38 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+function App(){
+
+    function Hello(){
+        return "Hellom,sajkdsjkjkskj"
+    }
+
+    let x = 98989
+    return(
+      
+      <>
+      {x}
+        <h1>Hello Class</h1>
+        <p>Welcome class {Hello()}</p>
+        </>
+    )
+}
+
+export default App;
+
+
